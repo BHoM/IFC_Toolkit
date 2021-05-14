081377b83c1705a282e5d1449bbc1a299acbf2eb
Pawel Baran <pawel.baran@burohappold.com>
2021-05-14 18:22:21 +0200

    oM descriptions added

--- IFC_oM/Misc/IFCRepresentation.cs
+++ IFC_oM/Misc/IFCRepresentation.cs
@@ -1,17 +1,42 @@
-﻿using BH.oM.Base;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+ 
+using BH.oM.Base;
 using BH.oM.Geometry;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BH.oM.Adapters.IFC
 {
+    [Description("Representation of an object extracted from an .ifc file.")]
     public class IFCRepresentation : IFragment, IImmutable
     {
-        //[Description("Meshed surfaces of Revit element represented by the BHoM object carrying this fragment.")]
+        /***************************************************/
+        /****             Public Properties             ****/
+        /***************************************************/
+
+        [Description("Meshes representing the object extracted from an .ifc file.")]
         public virtual ReadOnlyCollection<Mesh> Meshes { get; } = null;
 
 
